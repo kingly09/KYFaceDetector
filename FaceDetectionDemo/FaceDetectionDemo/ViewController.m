bb43ed62ee726ebd8c34e37125829d070bc22f21
kingly <libintm@163.com>
2018-02-08 15:46:49 +0800

    添加相册

--- FaceDetectionDemo/FaceDetectionDemo/ViewController.m
+++ FaceDetectionDemo/FaceDetectionDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "KYFaceViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
@@ -30,8 +30,51 @@
 
 - (IBAction)onClickSelfPortraits:(id)sender {
   
+  [self openCamera];
   
 }
+
+/**
+ * @brief 打开相机
+ */
+-(void)openCamera
+{
+  //先设定sourceType为相机，然后判断相机是否可用（ipod）没相机，不可用将sourceType设定为相片库
+  UIImagePickerControllerSourceType sourceType;
+  if (![UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera]) {
+    sourceType = UIImagePickerControllerSourceTypePhotoLibrary;//图片库
+  }else{
+    sourceType = UIImagePickerControllerSourceTypeCamera;//照相机
+  }
+  UIImagePickerController *picker = [[UIImagePickerController alloc] init];//初始化
+  picker.delegate = self;
+  picker.allowsEditing = NO;//设置不可编辑（无编辑框模式）
+  picker.navigationBar.barStyle = UIBarStyleDefault;
+  picker.navigationBar.barTintColor = [UIColor whiteColor];
+  picker.sourceType = sourceType;
+  picker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+  [self presentViewController:picker animated:YES completion:^{}];//进入照相界面
+}
+
+
+/**
+ * @brief 获取图片
+ */
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+  [picker dismissViewControllerAnimated:YES completion:^{}];
+  UIImage *picture = [info objectForKey:UIImagePickerControllerOriginalImage];
+  
+  _imageView.image = picture;
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+  //设置statusb的样式
+  [[UINavigationBar appearance] setBarStyle:UIBarStyleBlackTranslucent];
+  [picker dismissViewControllerAnimated:YES completion:^{}];
+}
+
 
 - (IBAction)onClickFaceDetect:(id)sender {
   
