1fc02363463f4e9e0b6d451791b0228888c35142
kingly <libintm@163.com>
2018-02-08 15:37:40 +0800

    动画执行

--- FaceDetectionDemo/FaceDetectionDemo/KYFaceAnimationView.m
+++ FaceDetectionDemo/FaceDetectionDemo/KYFaceAnimationView.m
@@ -13,6 +13,8 @@
   
   UILabel *animationLabel; //动作说明
   UIImageView *animationImageView; //动画图片
+  NSTimer *animationTimer;  //张嘴动画
+  BOOL  isOpenMouth;   //是否是张嘴
   
 }
 @end
@@ -26,6 +28,13 @@
     [self setupCustomView];
   }
   return self;
+}
+
+-(void)dealloc {
+  
+  NSLog(@"KYFaceAnimationView dealloc");
+  [self invalidateTimer];
+  
 }
 
 //自定义
@@ -49,6 +58,8 @@
 
 -(void)showAnimationLabel:(FaceAnimationType )faceAnimationType {
   
+  isOpenMouth = NO;
+  
   NSString *animationStr = @"";
   if (faceAnimationType == FaceAnimationTypeDefault ) {
       animationStr = @"请正视屏幕";
@@ -62,19 +73,75 @@
    CGRect titleLabelRect = [animationStr boundingRectWithSize:CGSizeMake(self.frame.size.width - 40,CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]} context:nil];
   animationLabel.text = animationStr;
   animationLabel.frame = CGRectMake( (self.frame.size.width - (titleLabelRect.size.width + 20))/2, 0, titleLabelRect.size.width + 20, 40);
-  
-  
-  animationImageView.frame = CGRectMake((self.frame.size.width - 150)/2, 55, 150, 150);
-  
+
   if (faceAnimationType == FaceAnimationTypeDefault ) {
     animationImageView.image  = [UIImage imageNamed:@"ic_home_face_default"];
+    animationImageView.frame = CGRectMake((self.frame.size.width - 150)/2, 55, 150, 150);
+    
+    [self invalidateTimer];
+    
   }else if (faceAnimationType == FaceAnimationTypeOpenMouth ) {
     animationImageView.image  = [UIImage imageNamed:@"ic_home_face_mouth"];
+    isOpenMouth = YES;
+    [UIView animateWithDuration:0.5 animations:^{
+        animationImageView.frame = CGRectMake(-150, 55, 150, 150);
+    } completion:^(BOOL finished) {
+       animationImageView.frame = CGRectMake((self.frame.size.width - 150)/2, 55, 150, 150);
+      
+      [self showOpenMouthAnimation];
+      
+    }];
+    
+    
   }else{
+    animationImageView.frame = CGRectMake((self.frame.size.width - 150)/2, 55, 150, 150);
     animationImageView.image  = [UIImage imageNamed:@"ic_home_face_default"];
+    
+    [self invalidateTimer];
   }
 
 }
 
 
+/**
+ 显示张嘴动画
+ */
+-(void)showOpenMouthAnimation {
+  
+
+  animationTimer = [NSTimer timerWithTimeInterval:0.5 target:self selector:@selector(runLoopOpenMouthAnimation) userInfo:nil repeats:YES];
+  [animationTimer fire];
+  [[NSRunLoop mainRunLoop] addTimer:animationTimer forMode:NSDefaultRunLoopMode];
+  
+  
+}
+
+-(void)runLoopOpenMouthAnimation {
+  
+
+  
+  if ( isOpenMouth == YES) {
+    NSLog(@"闭合");
+     isOpenMouth = NO;
+     animationImageView.image  = [UIImage imageNamed:@"ic_home_face_default"];
+  }else{
+     NSLog(@"张开");
+    isOpenMouth = YES;
+    animationImageView.image  = [UIImage imageNamed:@"ic_home_face_mouth"];
+  }
+  
+}
+
+/**
+ 注销定时器
+ */
+- (void)invalidateTimer {
+  
+  if ([animationTimer isValid]) {
+    [animationTimer invalidate];
+    animationTimer = nil;
+  }
+   isOpenMouth = NO;
+  
+}
 @end
