2b5b5238e84d3849bbffbe17ced9326380da75f6
kingly <libintm@163.com>
2018-02-12 11:49:33 +0800

    删除多余的工程

--- FaceDetectionDemo2/Pods/Target Support Files/Pods-FaceDetectionDemo/Pods-FaceDetectionDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FaceDetectionDemo : NSObject
-@end
-@implementation PodsDummy_Pods_FaceDetectionDemo
-@end
--- FaceDetectionDemo2/Pods/Target Support Files/Pods-FaceDetectionDemoUITests/Pods-FaceDetectionDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FaceDetectionDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FaceDetectionDemoUITests
-@end
