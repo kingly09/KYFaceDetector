e85665693b350bd397e4745c84bbdd6c280f809e
kingly <libintm@163.com>
2018-02-28 16:20:38 +0800

    优化

---
+++ BCFaceSDKDemo/face/face.h
@@ -0,0 +1,13 @@
+//
+//  face.h
+//  face
+//
+//  Created by kingly on 2018/2/28.
+//  Copyright © 2018年 Bambooclound Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface face : NSObject
+
+@end
---
+++ BCFaceSDKDemo/face/face.m
@@ -0,0 +1,13 @@
+//
+//  face.m
+//  face
+//
+//  Created by kingly on 2018/2/28.
+//  Copyright © 2018年 Bambooclound Co., Ltd. All rights reserved.
+//
+
+#import "face.h"
+
+@implementation face
+
+@end
